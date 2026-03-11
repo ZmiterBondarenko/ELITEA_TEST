e6565c5b14fa061dc67a9a5d520c4e793dbbbf76
Dmitry Bondarenko <zmiter.bondarenko@gmail.com>
2026-03-11 18:02:01 +0300

    Create tests/elitea-docs.spec.ts

---
+++ tests/elitea-docs.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('ELITEA site navigation', () => {
+  test('go to docs from homepage', async ({ page }) => {
+    // TODO: test steps will be added
+  });
+});
